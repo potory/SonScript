6ca1c016421ea003317c9643bd01a7385aca7227
Alexey Potory <>
2023-03-14 16:59:22 +0500

    Фикс фабрики функций

--- SonScript.Core/FunctionFactory.cs
+++ SonScript.Core/FunctionFactory.cs
@@ -8,14 +8,24 @@ namespace SonScript.Core;
 public class FunctionFactory
 {
     private readonly IServiceProvider _provider;
+    private readonly Dictionary<string, Type> _register = new();
+
     private readonly Dictionary<Type, Function> _cache = new();
 
     public FunctionFactory(IServiceProvider provider) => 
         _provider = provider;
 
+    public void RegisterFunction<T>(string name) where T : Function => 
+        _register.Add(name.ToLower(), typeof(T));
+
     public Function CreateFunction(string functionName)
     {
         var callName = functionName.ToLower();
+
+        if (_register.TryGetValue(callName, out var type))
+        {
+            return CreateFunction(type);
+        }
 
         return callName switch
         {
@@ -32,9 +42,11 @@ public class FunctionFactory
         };
     }
 
-    private Function CreateFunction<T>() where T: Function
+    private Function CreateFunction<T>() where T: Function => 
+        CreateFunction(typeof(T));
+
+    private Function CreateFunction(Type type)
     {
-        var type = typeof(T);
         var isCachingAllowed = IsCachingAllowed(type);
 
         if (!isCachingAllowed)
